104ea28e8f1f5f4a89af5148aa28d26d2165ebee
mithridatem <mathieumithridate@adrar-formation.com>
2024-01-26 14:42:51 +0100

    fix : hash

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -39,11 +39,12 @@ class RegisterType extends AbstractType
                 ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
-                'hash_property_path' => 'password',
+                
                 'invalid_message' => 'Les mots de passe ne correspondent pas.',
                 'options' => ['attr' => ['class' => 'password-field']],
                 'required' => true,
-                'first_options'  => ['label' => 'Mot de passe :'],
+                'first_options'  => ['label' => 'Mot de passe :',
+                'hash_property_path' => 'password',],
                 'second_options' => ['label' => 'Confirmer le mot de passe'],
             ])
             ->add('Ajouter',SubmitType::class)
