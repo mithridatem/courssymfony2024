54a58eadbc3381610a0c1e241bd6e88b64ee2e37
mithridatem <mathieumithridate@adrar-formation.com>
2024-01-26 14:41:39 +0100

    feat: form enregistrement

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class,[
+                'attr'=>['class'=>''],
+                'required'=>true,
+                'label'=> 'Saisir votre prénom :',
+                'label_attr'=>['class'=>'']
+                ]
+            )
+            ->add('lastname', TextType::class,[
+                'attr'=>['class'=>''],
+                'required'=>true,
+                'label'=> 'Saisir votre nom :',
+                'label_attr'=>['class'=>'']
+                ]
+            )
+            ->add('email', EmailType::class,[
+                'attr'=>['class'=>''],
+                'required'=>true,
+                'label'=> 'Saisir votre email :',
+                'label_attr'=>['class'=>'']
+                ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'hash_property_path' => 'password',
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe :'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
+            ])
+            ->add('Ajouter',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
