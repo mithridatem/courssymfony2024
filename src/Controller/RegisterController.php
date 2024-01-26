3725ae60e0f9ce2417c17322cf12e1a8261f695f
mithridatem <mathieumithridate@adrar-formation.com>
2024-01-26 16:20:34 +0100

    feat : formulaire avec tailwindcss

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,15 +4,45 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Form\RegisterType;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class RegisterController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function index(): Response
+    #[Route('/register/add', name: 'app_register_add')]
+    public function addUser(Request $request,UserPasswordHasherInterface $hasher, 
+        UserRepository $repo,EntityManagerInterface $em): Response
     {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted()AND $form->isValid()){
+            //test si l'utilisateur n'existe pas
+            if(!$repo->findOneBy(["email"=>$user->getEmail()])){
+                $type = "success";
+                $msg = "Le compte a été ajouté en BDD";
+                $password = $user->getPassword();
+                $hash = $hasher->hashPassword($user,$password);
+                $user->setPassword($hash);
+                //version en une seule étape
+                //$user->setPassword($hasher->hashPassword($user,$user->getPassword()));
+                $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+                $em->persist($user);
+                $em->flush();
+            }
+            //test si il existe
+            else{
+                $type = "danger";
+                $msg = "Les informations sont incorrectes";
+            }
+            $this->addFlash($type,$msg);
+        }
         return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -17,38 +17,49 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname', TextType::class,[
-                'attr'=>['class'=>''],
-                'required'=>true,
-                'label'=> 'Saisir votre prénom :',
-                'label_attr'=>['class'=>'']
+            ->add(
+                'firstname',
+                TextType::class,
+                [
+                    'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'],
+                    'required' => true,
+                    'label' => 'Saisir votre prénom :',
+                    'label_attr' => ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-white']
                 ]
             )
-            ->add('lastname', TextType::class,[
-                'attr'=>['class'=>''],
-                'required'=>true,
-                'label'=> 'Saisir votre nom :',
-                'label_attr'=>['class'=>'']
+            ->add(
+                'lastname',
+                TextType::class,
+                [
+                    'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'],
+                    'required' => true,
+                    'label' => 'Saisir votre nom :',
+                    'label_attr' => ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-white']
                 ]
             )
-            ->add('email', EmailType::class,[
-                'attr'=>['class'=>''],
-                'required'=>true,
-                'label'=> 'Saisir votre email :',
-                'label_attr'=>['class'=>'']
-                ])
+            ->add('email', EmailType::class, [
+                'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'],
+                'required' => true,
+                'label' => 'Saisir votre email :',
+                'label_attr' => ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-white']
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
-                
                 'invalid_message' => 'Les mots de passe ne correspondent pas.',
-                'options' => ['attr' => ['class' => 'password-field']],
+                'options' => ['attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']],
                 'required' => true,
-                'first_options'  => ['label' => 'Mot de passe :',
-                'hash_property_path' => 'password',],
-                'second_options' => ['label' => 'Confirmer le mot de passe'],
+                'first_options'  => [
+                    'label' => 'Mot de passe :',
+                    'label_attr' => ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-white']
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                    'label_attr' => ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-white']
+                ],
             ])
-            ->add('Ajouter',SubmitType::class)
-        ;
+            ->add('Ajouter', SubmitType::class, [
+                'attr' => ['class' => 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
