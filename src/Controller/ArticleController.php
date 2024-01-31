c656b06ed1b27b0203687d79d370f53c2bb12fae
mithridatem <mathieumithridate@adrar-formation.com>
2024-01-31 17:05:57 +0100

    feat: ajouter un article partie 1

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ArticleRepository;
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 class ArticleController extends AbstractController
 {
     private ArticleRepository $articleRepository;
@@ -18,7 +25,7 @@ class ArticleController extends AbstractController
     #[Route('/article/all', name: 'app_article_all')]
     public function showAllArticles(): Response {
         $articles = $this->articleRepository->findAll();    
-        return $this->render('article/index.html.twig',[
+        return $this->render('article/article_all.html.twig',[
             'articles' => $articles,
         ]);
     }
@@ -28,6 +35,43 @@ class ArticleController extends AbstractController
         $article = $this->articleRepository->find($id);
         return $this->render('article/article_id.html.twig',[
             'article' => $article,
+        ]);
+    }
+    #[Route('/article/add', name: 'app_article_add')]
+    public function addArticle(Request $request,EntityManagerInterface $em,
+    ArticleRepository $repo,UserInterface $userInterface, UserRepository $userRepository): Response {
+        //instanciation de l'entité Article
+        $article = new Article();
+        //création du formulaire
+        $form = $this->createForm(ArticleType::class,$article);
+        //recupération des données
+        $form->handleRequest($request);
+        //test de soumission et de validité du formulaire
+        if($form->isSubmitted() AND $form->isValid()){
+            //test si les champs sont remplis
+            if($form->getData()->getTitle()AND $form->getData()->getContent()
+            AND $form->getData()->getCreationDate()){
+                //test si l'article n'existe pas déja
+                if(!$repo->findOneBy(["title"=>$form->getData()->getTitle(), 
+                "content"=>$form->getData()->getContent()])){
+                    $article->setUser($userRepository->findOneBy(["email"=>$userInterface->getUserIdentifier()]));
+                    $em->persist($article);
+                    $em->flush();
+                    dd($article);
+                }
+                //test si il existe déja
+                else{
+                    dd("l'article existe déja");
+                }
+            }
+            //test si les champs sont vides
+            else{
+                dd("Veuillez remplir tous les champs");
+            }
+        }
+        //return de la vue twig
+        return $this->render('article/article_add.html.twig',[
+            'form'=> $form->createView(),
         ]);
     }
 }
---
+++ src/Form/ArticleType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title',TextType::class)
+            ->add('content',TextareaType::class)
+            ->add('creationDate',DateTimeType::class,[
+                'html5' => true,
+                'widget' => 'single_text',
+            ])
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => false,
+            ])
+            ->add('Ajouter',SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
