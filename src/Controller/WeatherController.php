3a3b076d90062c492d3eff9f95dac41cda65476a
mithridatem <mathieumithridate@adrar-formation.com>
2024-02-05 19:46:58 +0100

    feat: WeatherService => getWeatherCity and WeatherController => showWeatherCity, form => WeatherType and template twig

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -6,6 +6,8 @@ use App\Service\WeatherService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\WeatherType;
+use Symfony\Component\HttpFoundation\Request;
 
 class WeatherController extends AbstractController
 {
@@ -19,8 +21,31 @@ class WeatherController extends AbstractController
     {   
         //récupération du tableau de données depuis WeatherService
         $weather = $this->weatherService->getWeather();
+        //retour de la vue
         return $this->render('weather/show_weather.html.twig', [
             'weather' => $weather,
+        ]);
+    }
+    //méthode qui retourne la météo courante pour une ville donnée
+    #[Route('/weather/show/city', name: 'app_weather_show_city')]
+    public function showWeatherCity(Request $request): Response
+    {   
+        //création du formulaire
+        $form = $this->createForm(WeatherType::class);
+        //récupération des données du formulaire
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //récupération de la ville
+            $city = $form->getData()['city'];
+            //récupération du tableau de données depuis WeatherService
+            $weather = $this->weatherService->getWeatherCity($city);
+            //reconstruction du formulaire => (vider le champ ville)
+            $form = $this->createForm(WeatherType::class);
+        }
+        //retour de la vue
+        return $this->render('weather/show_weather_city.html.twig', [
+            'form' => $form->createView(),
+            'weather' => $weather??null,
         ]);
     }
 }
--- src/Service/WeatherService.php
+++ src/Service/WeatherService.php
@@ -27,10 +27,32 @@ class WeatherService
             'https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid='
                 . $this->apiKey
         );
-        //récupération du json
-        //$data =  $response->getContent();
         //récupération au format tableau
         $data = $response->toArray();
+        //retour du tableau
+        return $data;
+    }
+    public function getWeatherCity(string $city): array
+    {
+        //récupération du tableau de données depuis WeatherService
+        try {
+            //stocker le résultat de la requête dans $response
+            $response = $this->client->request(
+                'GET',
+                'https://api.openweathermap.org/data/2.5/weather?q='
+                    . $city . '&appid=' . $this->apiKey
+            );
+            //récupération au format tableau
+            $data = $response->toArray();
+        } 
+        //gestion des erreurs
+        catch (\Throwable $th) {
+            //si la ville n'est pas trouvée
+            if ($th->getCode() == 404) {
+                //retour d'un tableau avec un message d'erreur
+                $data = ['error' => $city.' non trouvée'];
+            }
+        }
         //retour du tableau
         return $data;
     }
